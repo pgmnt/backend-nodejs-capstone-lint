d56ca2b7eeaafa4001408fefcc7f21ac6ac6a645
devmezcode <hermeskii01@gmail.com>
2025-07-04 14:43:07 -0400

    Implementing Search Routes Logic

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
-
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
@@ -32,7 +31,7 @@ const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
-
+const searchRoutes = require('./routes/searchRoutes');
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -49,6 +48,7 @@ app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
+app.use('/api/secondchance/search', searchRoutes);
 
 
 // Global Error Handler
