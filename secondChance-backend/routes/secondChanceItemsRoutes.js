3426975d5998cfb92d28bc47d271ec1e44e15eac
devmezcode <hermeskii01@gmail.com>
2025-07-04 09:40:30 -0400

    Fix: Added req.params.id

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -71,12 +71,14 @@ router.post('/', upload.single('file'), async(req, res,next) => {
 // Get a single secondChanceItem by ID
 router.get('/:id', async (req, res, next) => {
     try {
+        
         //Step 4: task 1 - insert code here
         const db = await connectToDatabase();
         //Step 4: task 2 - insert code here
         const collection = db.collection("secondChanceItems");
         //Step 4: task 3 - insert code here
-        const secondChanceItem = await collection.findOne({id : id});
+        const id = req.params.id;
+        const secondChanceItem = await collection.findOne({id: id});
         //Step 4: task 4 - insert code here
         if (!secondChanceItem) {
             return res.status(404).send("secondChanceItem not found");
@@ -160,7 +162,7 @@ router.delete('/:id', async(req, res,next) => {
         //Step 6: task 4 - insert code here
         await collection.deleteOne({ id });
         res.json({'deleted': 'success'});
-        
+
     } catch (e) {
         next(e);
     }
