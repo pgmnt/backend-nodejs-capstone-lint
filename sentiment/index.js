e0d41ec6786515ae19c09f40b4552dffab2bef2d
devmezcode <hermeskii01@gmail.com>
2025-07-05 09:33:45 -0400

    Small changes on code readability

--- sentiment/index.js
+++ sentiment/index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const logger = require('./logger');
 const expressPino = require('express-pino-logger')({ logger });
 // Task 1: import the natural library
-const natural = require('natural');
+const natural = require("natural");
 
 // Task 2: initialize the express server
 const app = express();
@@ -38,7 +38,7 @@ app.post('/sentiment', async (req, res) => {
         let sentiment = "neutral";
 
         // Task 5: set sentiment to negative or positive based on score rules
-        if(analysisResult > 0){
+        if(analysisResult > 0.33){
             sentiment = "positive";
         } else if(analysisResult < 0) {
             sentiment = "negative";
@@ -48,11 +48,11 @@ app.post('/sentiment', async (req, res) => {
         logger.info(`Sentiment analysis result: ${analysisResult}`);
 
         // Task 6: send a status code of 200 with both sentiment score and the sentiment txt in the format { sentimentScore: analysisResult, sentiment: sentiment }
-        return res.status(200).json({sentimentScore: analysisResult, sentiment: sentiment});
+        res.status(200).json({ sentimentScore: analysisResult, sentiment: sentiment });
     } catch (error) {
         logger.error(`Error performing sentiment analysis: ${error}`);
         // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
-        return res.status(500).json({message: 'Error performing sentiment analysis'});
+        res.status(500).json({ message: 'Error performing sentiment analysis' });
     }
 });
 
